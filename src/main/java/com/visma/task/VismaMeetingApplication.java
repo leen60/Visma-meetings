1e64b447ab22373c62db50a50e1379d4023b80aa
leen60 <laura.zilinskaite@outlook.com>
2022-02-01 00:42:07 +0200

    Project init;

---
+++ src/main/java/com/visma/task/VismaMeetingApplication.java
@@ -0,0 +1,13 @@
+package com.visma.task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VismaMeetingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VismaMeetingApplication.class, args);
+    }
+
+}
